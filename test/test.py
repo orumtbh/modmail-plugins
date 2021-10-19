d6d4665dd7a74e9c35037b007a1a01a246002f64
orumtbh <92748134+orumtbh@users.noreply.github.com>
2021-10-19 15:55:46 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -7,29 +7,11 @@ import time
 from datetime import datetime
 from discord.ext import commands
 
-client = Bot (description="DESCRIPTION", command_prefix="PREFIX")
-
-@client.event
-async def on_ready():
-	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | '+str(len(client.servers))+' servers')
-	print(discord.__version__)
-	await client.change_presence(game=discord.Game(name="Just ping me!"))
-	
-@client.event
-async def on_message(message):
-	possible = ["<@!506158857098362890>", "<@506158857098362890>"]
-	author = message.author
-	server = message.server
-	giverole = discord.utils.get(server.roles, id='506157422574829587')
-	if message.content in possible:
-		if 'Giveaway' in [y.name for y in message.author.roles]:
-			await client.remove_roles(author, giverole)
-			await client.send_message(message.channel, "{}, I've unsubscribed you from the giveaway pings.".format(author.mention))
-		else:
-			await client.add_roles(author, giverole)
-			await client.send_message(message.channel, "{}, I've subscribed you to the giveaway pings.".format(author.mention))
-	else:
-		pass
-		 
-
-client.run('TOKEN')
+    @bot.command(pass_context=True)
+    @commands.has_role("Admin")
+    async def unmute(ctx, user: discord.Member):
+        role = discord.utils.find(lambda r: r.name == 'Yaksha', ctx.message.server.roles)
+        if role in user.roles:
+            await bot.say("{} is not muted".format(user))
+        else:
+            await bot.add_roles(user, role)
